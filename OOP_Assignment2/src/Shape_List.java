8abb32581fc9240341b6fd0d911480c841268c3f
Chin khuslen <ssjbuniverse7@gmail.com>
2026-06-23 16:46:01 +0100

    New version

---
+++ OOP_Assignment2/src/Shape_List.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+class Shape_List {
+    private ArrayList<Shape> listOfShapes;
+
+    public Shape_List() {
+        listOfShapes = new ArrayList<>();
+    }
+
+    public void addShape(Shape s) {
+        listOfShapes.add(s);
+    }
+
+    public void translateShapes(int dx, int dy) {
+        for (Shape each : listOfShapes) {
+            each.translate(dx, dy);
+        }
+    }
+
+    public Shape getShape(int id) {
+        return listOfShapes.get(id - 1);
+    }
+
+    public Shape removeShape(int id) {
+        return listOfShapes.remove(id - 1);
+    }
+
+    public double area(int id) {
+        return listOfShapes.get(id - 1).getArea();
+    }
+
+    public void scale(int factor, boolean sign) {
+        for (Shape each : listOfShapes) {
+            each.scale(factor, sign);
+        }
+    }
+
+    public double perimeter(int id) {
+        return listOfShapes.get(id - 1).getPerimeter();
+    }
+
+    public String display() {
+        StringBuilder displayString = new StringBuilder();
+        int id = 1;
+        for (Shape each : listOfShapes) {
+            displayString.append("\nID (Required to find a shape) : ").append(id).append("\n");
+            displayString.append(each.display()).append("\n");
+            id++;
+        }
+        return displayString.toString();
+    }
+
+    public int size() {
+        return listOfShapes.size();
+    }
+
+    public boolean isValid(int id) {
+        return id > 0 && id <= listOfShapes.size();
+    }
+}
