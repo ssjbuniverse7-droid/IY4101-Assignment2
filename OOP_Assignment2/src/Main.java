8743f12a81bb9c02611aaec07dfd4aa24c5b2341
Chin khuslen <ssjbuniverse7@gmail.com>
2026-06-29 02:31:32 +0100

    Run program from Main

--- OOP_Assignment2/src/Main.java
+++ OOP_Assignment2/src/Main.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-       
+        System.out.println("\n***** Shapes Application *****");
+        Shape_Management manager = new Shape_Management();
+        manager.run();
     }
 }
