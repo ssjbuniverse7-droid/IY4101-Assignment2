633ef2667d6bf997535073b79584e26d14c3448e
Chin khuslen <ssjbuniverse7@gmail.com>
2026-06-29 02:31:58 +0100

    Refactor Rectangle class to include width property

--- OOP_Assignment2/src/Rectangle.java
+++ OOP_Assignment2/src/Rectangle.java
@@ -1,35 +1,42 @@
 public class Rectangle extends Shape {
     // Properties
-    private double width;
     private double length;
+    private double width;
+
     // Constructor
-    public Rectangle (Coordinates coord, double side) {
+    public Rectangle(Coordinates coord, double length, double width) {
         super(4, coord);
         this.length = length;
         this.width = width;
     }
 
+    // Scale The Rectangle
     @Override
     public void scale(int factor, boolean sign) {
         if (sign) {
-            this.length *= factor;
+            length *= factor;
+            width *= factor;
         } else {
-            this.length /= factor;
+            length /= factor;
+            width /= factor;
         }
     }
 
+    // Calculate Area
     @Override
     public double getArea() {
-        return Math.PI * length * width;
+        return length * width;
     }
 
+    // Calculate Perimeter
     @Override
     public double getPerimeter() {
-        return (2 * length) + (2 * width);
+        return 2 * (length + width);
     }
 
+    // Display Info
     @Override
     public String display() {
-        return "Rectangle at " + position.display() + ", length: " + length;
+        return "Rectangle at " + position.display() + ", Length: " + length + ", Width: " + width;
     }
 }
